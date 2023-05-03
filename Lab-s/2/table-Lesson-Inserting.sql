3e04facf1455717f1375cdfe9c68ce1d63a5b673
NemoNology <chicken.gang@mail.ru>
2023-05-03 20:50:25 +0900

    Added Inserting values into "Lesson" table

---
+++ Lab-s/2/table-Lesson-Inserting.sql
@@ -0,0 +1,16 @@
+INSERT INTO "Lesson" VALUES
+( DEFAULT,'Математика', False, 88 ),
+( DEFAULT, 'Математика', True, 184 ),
+( DEFAULT, 'Русский язык и культура речи', False, 64 ),
+( DEFAULT, 'Программирование', False, 48 ),
+( DEFAULT, 'Программирование', True, 208 ),
+( DEFAULT, 'Иностранный язык', False, 88 ),
+( DEFAULT, 'Иностранный язык', True, 88 ),
+( DEFAULT, 'Структуры и алгоритмы обработки данных', False, 88 ),
+( DEFAULT, 'Структуры и алгоритмы обработки данных', True, 184 ),
+( DEFAULT, 'Базовая физика', False, 88 ),
+( DEFAULT, 'Базовая физика', True, 88 ),
+( DEFAULT, 'История', False, 64 ),
+( DEFAULT, 'История', True, 32 );
+
+SELECT * FROM "Lesson"
