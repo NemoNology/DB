067e502bc34ad9ac1ba4a3190c128d8d9338063a
NemoNology <chicken.gang@mail.ru>
2023-05-04 04:51:45 +0900

    Added Inserting values into "Group" table

---
+++ Lab-s/2/table-Group-Inserting.sql
@@ -0,0 +1,11 @@
+INSERT INTO "Group" VALUES 
+( DEFAULT, 'ИВТ-20' ),
+( DEFAULT, 'ТГК-20' ),
+( DEFAULT, 'ВМК-20' ),
+( DEFAULT, 'ИВТ-19' ),
+( DEFAULT, 'ТГК-19' ),
+( DEFAULT, 'ВМК-19' );
+
+
+
+SELECT * FROM "Group"
