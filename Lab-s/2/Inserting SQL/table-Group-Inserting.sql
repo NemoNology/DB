54c5247d3e4c187084819282c96d3a99bb6a309c
NemoNology <chicken.gang@mail.ru>
2023-08-17 18:22:05 +0900

    Replaced files + Added deleting old values

---
+++ Lab-s/2/Inserting SQL/table-Group-Inserting.sql
@@ -0,0 +1,25 @@
+-- DELETE FROM "Group" *;
+
+-- TRUNCATE TABLE "Group" RESTART IDENTITY;
+
+INSERT INTO "Group" VALUES 
+( DEFAULT, 'ИСТ-15' ),
+( DEFAULT, 'МАТ-15' ),
+( DEFAULT, 'ФИЗ-18' ),
+( DEFAULT, 'ХИМ-16' ),
+( DEFAULT, 'БИО-16' ),
+( DEFAULT, 'ГЕО-16' ),
+( DEFAULT, 'ЭКО-19' ),
+( DEFAULT, 'ПРА-17' ),
+( DEFAULT, 'СОЦ-19' ),
+( DEFAULT, 'ПСИ-15' ),
+( DEFAULT, 'ФИЛ-14' ),
+( DEFAULT, 'ЛИН-17' ),
+( DEFAULT, 'ИСК-18' ),
+( DEFAULT, 'МЕД-16' ),
+( DEFAULT, 'ПЕД-14' ),
+( DEFAULT, 'СПО-15' );
+
+
+
+SELECT * FROM "Group"
---
+++ Lab-s/2/Inserting SQL/table-Load-Inserting.sql
@@ -0,0 +1,22 @@
+-- DELETE FROM "Load" *;
+
+-- TRUNCATE TABLE "Load" RESTART IDENTITY;
+
+INSERT INTO "Load" VALUES
+( DEFAULT, '2020-01-01', 2, 1, 2 ),
+( DEFAULT, '2020-01-01', 2, 2, 2 ),
+( DEFAULT, '2020-01-01', 2, 3, 2 ),
+( DEFAULT, '2019-01-01', 2, 4, 2 ),
+( DEFAULT, '2019-01-01', 2, 5, 2 ),
+( DEFAULT, '2019-01-01', 2, 6, 2 ),
+					  
+( DEFAULT, '2020-01-01', 4, 1, 5 ),
+( DEFAULT, '2020-01-01', 4, 3, 5 ),
+( DEFAULT, '2019-01-01', 4, 4, 5 ),
+( DEFAULT, '2019-01-01', 4, 6, 5 ),
+					  
+( DEFAULT, '2020-01-01', 7, 2, 11 ),
+( DEFAULT, '2019-01-01', 7, 5, 11 );
+
+
+SELECT * FROM "Load"
