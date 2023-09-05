03acbec8cb95ef3b1fd5bcb6921ba44cdc4e5575
NemoNology <chicken.gang@mail.ru>
2023-09-05 21:37:45 +0900

    Added specified solution - B 1.5

--- Lab-s/3/Select scripts/teachers amount for each group.sql
+++ Lab-s/3/Select scripts/teachers amount for each group.sql
@@ -1,6 +1,19 @@
 -- количество преподавателей для каждой группы по годам;
 -- the number of teachers for each group by year;
 
+SELECT "ID_Group", 
+	COUNT(DISTINCT pd."ID_Professor") AS "p_am",
+	"Semester number" AS sem_num
+
+FROM "Load" l
+
+JOIN "Professor - Discipline" pd ON l."ID_Professor - Discipline" = pd."ID"
+
+GROUP BY "ID_Group", sem_num
+
+ORDER BY "ID_Group"
+;
+
 WITH 
 devider
 AS
