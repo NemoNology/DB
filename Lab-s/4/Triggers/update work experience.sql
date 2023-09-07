32a84f36c01ac64ede05586329de1aff7b4b7213
NemoNology <chicken.gang@mail.ru>
2023-09-08 04:08:35 +0900

    Build 1.0

---
+++ Lab-s/4/Triggers/update work experience.sql
@@ -0,0 +1,25 @@
+-- Обновление стажа работы преподавателя в зависимости от возраста
+-- Update professor work experience by age
+
+
+-- trigger function
+CREATE OR REPLACE FUNCTION update_work_experience() 
+RETURNS TRIGGER AS
+$$
+BEGIN
+
+	IF (NEW."Age" - NEW."Work experience" < 14)
+		THEN NEW."Work experience" = NEW."Age" - 14;
+	END IF;
+	
+	RETURN NEW;
+
+END
+$$
+LANGUAGE plpgsql;
+
+-- creating trigger
+CREATE OR REPLACE TRIGGER on_update_work_experience
+	BEFORE INSERT OR UPDATE OF "Work experience" ON "Professor"
+	FOR EACH ROW 
+	EXECUTE PROCEDURE update_work_experience();
