2eaffcca760bed8f726f06c51e61da88559d9c6d
NemoNology <chicken.gang@mail.ru>
2023-09-09 07:02:39 +0900

    Fixed json output (Not working...)

--- Lab-s/Json/to json.sql
+++ Lab-s/Json/to json.sql
@@ -14,3 +14,45 @@ SELECT array_to_json(array_agg(row_to_json(t))) FROM
 
 	ORDER BY Name, "Semester number") 
 t;
+
+WITH 
+extra
+AS
+(
+	SELECT pd."ID_Professor" AS id_p, 
+		COUNT(l."ID") AS les_am, 
+		MAX("Lesson number") AS latest_les, 
+		"Semester number" AS sem_num
+	FROM "Load" l
+
+	JOIN "Professor - Discipline" pd ON l."ID_Professor - Discipline" = pd."ID"
+
+	GROUP BY pd."ID_Professor", "Semester number"
+
+	ORDER BY id_p, "Semester number"
+)
+
+SELECT info FROM
+( 
+	
+	SELECT json_build_object( 
+		'Name', p."ID",--p."Patronymic" || ' ' || p."First name",
+		'Semesters', json_agg(json_build_object( 
+			'Number', "Semester number",
+			'Info', json_build_object('Les am', les_am, 'Max', latest_les) 
+		)) 
+			 
+	) AS Info
+	FROM "Load" l
+
+	JOIN "Professor - Discipline" pd ON l."ID_Professor - Discipline" = pd."ID"
+	JOIN "Professor" p ON pd."ID_Professor" = p."ID"
+	JOIN extra e ON pd."ID_Professor" = e.id_p
+
+	GROUP BY p."ID"--p."Patronymic" || ' ' || p."First name"
+
+	ORDER BY p."ID"--p."Patronymic" || ' ' || p."First name"
+	
+	LIMIT 2
+
+) info;
