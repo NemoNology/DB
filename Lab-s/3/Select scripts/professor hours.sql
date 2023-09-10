0584af6fef20cee943882c4a2fed1bee62701c42
NemoNology <chicken.gang@mail.ru>
2023-09-10 23:28:55 +0900

    Fixed result - B 1.2

--- Lab-s/3/Select scripts/professor hours.sql
+++ Lab-s/3/Select scripts/professor hours.sql
@@ -1,12 +1,11 @@
 -- суммарное количество часов, запланированное на каждого преподавателя;
 -- the total number of hours planned for each teacher;
 
-SELECT "Patronymic", "First name", SUM(l."Number of hours") AS "Total hours"
+SELECT "Patronymic" || ' ' || "First name" AS "Name", SUM(l."Number of hours") AS "Total hours"
 FROM "Professor" p
 
 JOIN "Professor - Discipline" pd ON p."ID" = pd."ID_Professor"
-JOIN "Load" ld ON pd."ID" = ld."ID_Professor - Discipline"
 JOIN "Lesson" l ON pd."ID_Academic discipline" = l."ID"
 
-GROUP BY pd."ID_Professor", p."First name", p."Patronymic"
-ORDER BY p."Patronymic";
+GROUP BY "Patronymic" || ' ' || "First name"
+ORDER BY "Name";
