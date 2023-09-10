7e7ed54e4f98be2f20cc7580df6ab34e8e4b6cde
NemoNology <chicken.gang@mail.ru>
2023-09-10 18:17:40 +0900

    Reworked script - B 1.5

--- Lab-s/Json/to json.sql
+++ Lab-s/Json/to json.sql
@@ -1,58 +1,19 @@
-SELECT array_to_json(array_agg(row_to_json(t))) FROM 
-	(
-	SELECT p."Patronymic" || ' ' || p."First name" AS Name,
-		COUNT(l."ID") AS "Lessons amount by semester",
-		MAX("Lesson number") AS "The latest lesson by time at semester",
-		"Semester number"
-
-	FROM "Load" l
-
-	JOIN "Professor - Discipline" pd ON l."ID_Professor - Discipline" = pd."ID"
-	JOIN "Professor" p ON pd."ID_Professor" = p."ID"
-
-	GROUP BY p."Patronymic" || ' ' || p."First name", "Semester number"
-
-	ORDER BY Name, "Semester number") 
-t;
-
-WITH 
-extra
-AS
-(
-	SELECT pd."ID_Professor" AS id_p, 
-		COUNT(l."ID") AS les_am, 
-		MAX("Lesson number") AS latest_les, 
-		"Semester number" AS sem_num
-	FROM "Load" l
-
-	JOIN "Professor - Discipline" pd ON l."ID_Professor - Discipline" = pd."ID"
-
-	GROUP BY pd."ID_Professor", "Semester number"
-
-	ORDER BY id_p, "Semester number"
-)
-
-SELECT info FROM
+SELECT json_agg(info) FROM
 ( 
-	
 	SELECT json_build_object( 
-		'Name', p."ID",--p."Patronymic" || ' ' || p."First name",
-		'Semesters', json_agg(json_build_object( 
-			'Number', "Semester number",
-			'Info', json_build_object('Les am', les_am, 'Max', latest_les) 
-		)) 
-			 
-	) AS Info
-	FROM "Load" l
+		'Name', "Patronymic" || ' ' || "First name", 
+		'Lessons', json_agg(json_build_object( 
+				'Name', les."Name",
+				'Number of hours', les."Number of hours"
+			) 
+		) 
+	) AS "Professors info"
+	FROM "Professor" p	
 
-	JOIN "Professor - Discipline" pd ON l."ID_Professor - Discipline" = pd."ID"
-	JOIN "Professor" p ON pd."ID_Professor" = p."ID"
-	JOIN extra e ON pd."ID_Professor" = e.id_p
+	JOIN "Professor - Discipline" pd ON p."ID" = pd."ID_Professor"
+	JOIN "Lesson" les ON pd."ID_Academic discipline" = les."ID"
 
-	GROUP BY p."ID"--p."Patronymic" || ' ' || p."First name"
-
-	ORDER BY p."ID"--p."Patronymic" || ' ' || p."First name"
-	
-	LIMIT 2
+	GROUP BY "Patronymic" || ' ' || "First name"
+	ORDER BY "Patronymic" || ' ' || "First name"
 
 ) info;
