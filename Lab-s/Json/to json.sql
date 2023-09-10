a5432612ea61f4d2f43cfc4f60e73ad747ca9851
NemoNology <chicken.gang@mail.ru>
2023-09-11 03:16:15 +0900

    Fixed lessons output

--- Lab-s/Json/to json.sql
+++ Lab-s/Json/to json.sql
@@ -2,7 +2,7 @@ SELECT json_agg(info) FROM
 ( 
 	SELECT json_build_object( 
 		'Name', "Patronymic" || ' ' || "First name", 
-		'Lessons', json_agg(json_build_object( 
+		'Lessons (lecture)', json_agg(json_build_object( 
 				'Name', les."Name",
 				'Number of hours', les."Number of hours"
 			) 
@@ -13,7 +13,11 @@ SELECT json_agg(info) FROM
 	JOIN "Professor - Discipline" pd ON p."ID" = pd."ID_Professor"
 	JOIN "Lesson" les ON pd."ID_Academic discipline" = les."ID"
 
+	WHERE les."Type" = False
+
 	GROUP BY "Patronymic" || ' ' || "First name"
 	ORDER BY "Patronymic" || ' ' || "First name"
+
+	LIMIT 3
 
 ) info;
